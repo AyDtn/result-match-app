970f245c3172a62a423307e762750a5e9a14e1f3
AyDtn <dantanaymeric@gmail.com>
2025-04-15 20:24:47 +0200

    Remplacement complet avec la version propre

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Résultat du Match</h1>
+      <p>Votre formulaire et image iront ici.</p>
+    </div>
+  );
+}
+
+export default App;
